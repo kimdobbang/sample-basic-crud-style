b2344189ac1914701e4ec0dc18e0bd297f037e8a
kim doi <kimdoi.dev@gmail.com>
2025-01-18 16:50:34 +0900

    SQL 생성

---
+++ src/main/java/com/example/demo/board/domain/Board.java
@@ -0,0 +1,14 @@
+package com.example.demo.board.domain;
+
+import java.time.Instant;
+
+public class Board {
+    private Long id;
+    private String title;
+    private String content;
+    private String status;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+}
+
