41776a2a6e3c02f0eb6ef10199406cdd4a031a48
kim doi <kimdoi.dev@gmail.com>
2025-01-18 16:52:51 +0900

    application 수정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.crud;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrudApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
