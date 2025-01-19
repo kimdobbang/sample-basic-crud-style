9fe4a92c2facec86b3b00b98bf9a6b2b3acb9e27
kim doi <kimdoi.dev@gmail.com>
2025-01-19 14:11:27 +0900

    Feat: Board 모델

--- src/main/java/com/example/demo/board/domain/Board.java
+++ src/main/java/com/example/demo/board/domain/Board.java
@@ -1,14 +1,29 @@
 package com.example.demo.board.domain;
 
+import com.example.demo.board.domain.types.BoardStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.Instant;
 
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
-    private String status;
-    private Instant createdAt;
+    @Enumerated(EnumType.STRING) // 열거상수 네임 그대로 사용하겠다
+    private BoardStatus status;
+    private Instant createdAt; // snake_case -> CamelCase
     private Instant updatedAt;
+
+
 
 }
 
